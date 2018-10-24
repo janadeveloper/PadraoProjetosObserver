0fb13ccf279e465d9dc802a3b3d73a230e3d8c55
Janaine <janainesantosifal@gmail.com>
2018-10-24 09:10:48 -0300

    Criação das classes

---
+++ PadraoObserver/src/EstacaoMeteorologica.java
@@ -0,0 +1,15 @@
+
+public class EstacaoMeteorologica {
+	private float temperatura;
+	private float umidade;
+	private float pressao;
+	
+	public EstacaoMeteorologica(float temperatura, float umidade, float pressao) {
+		super();
+		this.temperatura = temperatura;
+		this.umidade = umidade;
+		this.pressao = pressao;
+	}
+	
+	
+}
